0ec8de4d3fc9b659b6b88aa1482a512ae8db6821
Mamurjon0001 <ibragimovmamurjon4@gmail.com>
2022-11-06 17:14:52 +0500

    Masalalar

--- index.js
+++ index.js
@@ -1,61 +1,206 @@
-var ism = prompt('Ismingizni kiriting');
-var OTM = prompt('OTM lar dan birini tanlang: 1.TDIU 2.TDMI 3.MU');
-var ball = prompt( ism, 'balingizni kiriting:');
+// 1-masala
+
+// var a = prompt('Nechi santimetr:');
+// var b = a /100;
+// alert(a +'santimetr ='+ b +'mga');
+
+
+// 2-masala
+
+// var a = prompt('Nechi kilogram:');
+// var b = a /1000;
+// alert(a +'kilogram ='+ b +'tga');
+
+// 3-masala
+
+// var n = prompt('Birinchi sonni kiriting');
+// var b = prompt('Ikkinchi sonni kiriting');
+
+// var c= n/b;
+
+// alert(c + 'marta joylashtirish mumkin');
+
+// 4-masala
+
+// var a = prompt('Sonni kiriting');
+// var b = a/10;
+// var c = a%10;
+// alert('birinchi son' + b);
+// alert('Ikkinchi son' + c);
+
+
+// 5-masala
+
+// var a = prompt('Sonni kiriting');
+// var b = a/10;
+// var c = a%10;
+// alert(b+c);
+
+// 6-masala
+
+// var a = prompt('Sonni kiriting');
+// var b = a/10;
+// var c = a%10;
+// alert(b*c);
+
+// 7-masala
+
+// var a = prompt('Sonni kiriting');
+
+// var b = a / 100;
+// alert(b);
+
+// 8-masala
+
+// var a = prompt('nechi sekund');
+// var m = a/60;
+// var s= a/3600;
+// alert(a+'sekund = ' + m + 'minutga');
+// alert(a+'sekund = ' + s + 'soatga');
+
+
+// 9-masala
+
+// var a= prompt('sonni kiriting');
+
+// if (a > 0){
+//     alert(a + 'soni musbat');
+// }
+
+// else if (a<0){
+//     alert(a+'soni manfiy');
+// }
+
+// else{
+//     alert('Bunday son mavjud emas')
+// }
+
+
+// 10-masala
+
+// var a = prompt('Sonni kiriting');
+
+// if(a%2==0){
+//     alert(a + 'soni juft');
+// }
+
+// else if (a%2!=0){
+//     alert(a + 'soni toq');
+// }
+
+// else{
+//     alert('Bunday son mavjud emas');
+// }
+
+// 11-masala
+
+// var a= prompt('Birinchi sonni kiriting');
+// var b= prompt('Ikkinchi sonni kiriting');
+
+// var c= a-b;
+
+// if (a>0 && b>0){
+//     alert(c);
+// }
+
+// else{
+//     alert('Xato son kiritilindi');
+// }
+
+// 12-masala
+
+// var a = prompt('A Sonni kiriting');
+// var b = prompt('B Sonni kiriting');
+// var c = prompt('C Sonni kiriting');
+
+// if(a>b&&b>c){
+//     alert('A>B>C')
+// }
+// else{
+//     alert('Sonlar ketma-ketligi xato');
+// }
+
+
+// 13-masala
+
+// var a = prompt('A Sonni kiriting');
+// var b = prompt('B Sonni kiriting');
+// var c = prompt('C Sonni kiriting');
+
+// if(a>b&&b<c){
+//     alert('B soni A sondan katta va C sondan kichik: A>B va B<A');
+// }
+// else{
+//     alert('Sonlar ketma-ketligi xato');
+// }
+
+// 14-masala
+
+// var a = prompt('A Sonni kiriting');
+// var b = prompt('B Sonni kiriting');
+
+// if(a%2!=0 ){
+//     alert('Kiritilgan son toq')
+// }
+// else if(b%2!=0){
+//     alert('Kiritilgan son toq');
+// }else{
+//     alert('Kiritilgan son toq emas');
+// }
+
+// 15-masala
+
+
+// var a = prompt('A Sonni kiriting');
+// var b = prompt('B Sonni kiriting');
+// var c = prompt('C Sonni kiriting');
+// var d = prompt('D Sonni kiriting');
+
+
+// if(a%2==0 && b%2==0 && c%2==0 && d%2==0){
+//     alert('Kiritilgan son juft');
+// }
+// else{
+//     alert('Kiritilgan son juft emas');
+// }
 
 
 
-
-var grant = 'Grant asosida oqishga tavsiya qilindingiz';
-var kantrakt = 'Kontrakt asosida oqishga tavsiya qilindingiz';
-var past = 'Oqishga tavsiya qilinmadingiz';
-var xato = 'Siz notgri malumot kiritdingiz';
+// 16-masala
 
 
+// var a = prompt('A Sonni kiriting');
+// var b = prompt('B Sonni kiriting');
+// var c = prompt('C Sonni kiriting');
 
 
-if (OTM == 1){
-    if(ball >= 150){
-        alert(grant);
-    }
-    else if( ball > 120){
-        alert(kantrakt);
-        
-    }
-    else if(120 >= ball > 0){
-        alert(past);
-        
-    }
-    else{
-        alert(xato);
-    }
-}else if(OTM == 2){
-    if(ball >= 160){
-        alert(grant);
-    }
-    else if(ball > 100){
-        alert(kantrakt);
-    }
-    else if(100 >= ball > 0){
-        alert( past);
-    }
-    else{
-        alert(xato);
-    }
-}else if(OTM == 3){
-    if(ball >= 180){
-        alert(grant);
-    }
-    else if( ball > 168){
-        // console.log(kantrakt);
-        alert(kantrakt);
-    }
-    else if(168 >= ball > 0){
-        // console.log(no);
-        alert( past);
-    }
-    else{
-        alert(xato);
-    }
-}else{
-    alert(xato);
-}
+
+// if(a%2==0 && b%2==0 | b%2==0 && c%2==0 | a%2==0 && c%2==0){
+//     alert('Kiritilgan son juft');
+// }
+// else{
+//     alert('Kiritilgan son juft emas');
+// }
+
+
+// 17-masala
+
+// var a  = prompt('Sonni kiriting');
+
+// if(a==1){
+//     alert('Dushanba');
+// }else if (a==2){
+//     alert('Seshanba');
+// }else if (a==3){
+//     alert('Chorshanba');
+// }else if (a==4){
+//     alert('Payshanba');
+// }else if (a==5){
+//     alert('Juma');
+// }else if(a==6){
+//     alert('Shanba');
+// }else if(a==7){
+//     alert('Yakshanba');
+// }else{
+//     alert('Xato son kiritdingiz');
+// }
