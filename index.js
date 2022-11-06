b0d3d88a87a5ec14417e4e694bfc0067cfd92835
Mamurjon0001 <ibragimovmamurjon4@gmail.com>
2022-11-06 14:44:23 +0500

    Javascript

---
+++ index.js
@@ -0,0 +1,60 @@
+var grant = 'Grant asosida oqishga tavsiya qilindingiz';
+var kantrakt = 'Kontrakt asosida oqishga tavsiya qilindingiz';
+var past = 'Oqishga tavsiya qilinmadingiz';
+var xato = 'Siz notgri malumot kiritdingiz';
+
+
+
+
+var ism = prompt('Ismingizni kiriting');
+
+var OTM = prompt('OTM lar dan birini tanlang: 1.TDIU 2.TDMI 3.MU');
+
+var ball = prompt( ism, 'balingizni kiriting:');
+
+if (OTM == 1){
+    if(ball >= 150){
+        alert(grant);
+    }
+    else if( ball > 120){
+        alert(kantrakt);
+        
+    }
+    else if(120 >= ball > 0){
+        alert(past);
+        
+    }
+    else{
+        alert(xato);
+    }
+}else if(OTM == 2){
+    if(ball >= 160){
+        alert(grant);
+    }
+    else if(ball > 100){
+        alert(kantrakt);
+    }
+    else if(100 >= ball > 0){
+        alert( noo);
+    }
+    else{
+        alert(erro);
+    }
+}else if(OTM == 3){
+    if(ball >= 180){
+        alert(grant);
+    }
+    else if( ball > 168){
+        // console.log(kantrakt);
+        alert(kantrakt);
+    }
+    else if(168 >= ball > 0){
+        // console.log(no);
+        alert( noo);
+    }
+    else{
+        alert(erro);
+    }
+}else{
+    alert(erro);
+}
