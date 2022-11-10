4e2801515c2bb1b450ace3e2adaeb1cb1b21e028
Mamurjon0001 <ibragimovmamurjon4@gmail.com>
2022-11-10 23:37:28 +0500

    resume

--- index.js
+++ index.js
@@ -1,206 +1,44 @@
-// 1-masala
+var getFullName = prompt('Full Name ?')
+var fullName = document.getElementsByClassName('box__right__title');
+fullName[0].innerHTML = getFullName;
 
-// var a = prompt('Nechi santimetr:');
-// var b = a /100;
-// alert(a +'santimetr ='+ b +'mga');
 
+var getjob = prompt('Who are you ?', "Programmer");
+var job = document.getElementsByClassName('job');
+job[0].innerHTML = getjob;
 
-// 2-masala
 
-// var a = prompt('Nechi kilogram:');
-// var b = a /1000;
-// alert(a +'kilogram ='+ b +'tga');
+var getExperience = prompt('In what kind of job did you worked and when?  Please write fully. All your job which you worked.');
+var experience = document.getElementsByClassName('experience');
 
-// 3-masala
+experience[0].innerHTML = getExperience;
 
-// var n = prompt('Birinchi sonni kiriting');
-// var b = prompt('Ikkinchi sonni kiriting');
+var getinformation = prompt('I have to know the basic things about you. Your biography.');
+var about = document.getElementsByClassName('about__me');
+about[0].innerHTML=getinformation;
 
-// var c= n/b;
 
-// alert(c + 'marta joylashtirish mumkin');
+var getPhoneNumber = prompt('Please write your Phone number');
+var number = document.getElementsByClassName('number__phone');
+number[0].innerHTML=getPhoneNumber;
 
-// 4-masala
+var getEmail = prompt('Please write your Email');
+var email = document.getElementsByClassName('number__email');
+email[0].innerHTML=getEmail;
 
-// var a = prompt('Sonni kiriting');
-// var b = a/10;
-// var c = a%10;
-// alert('birinchi son' + b);
-// alert('Ikkinchi son' + c);
+var getAdress = prompt('Please write your Current location');
+var adress = document.getElementsByClassName('number__adress');
+adress[0].innerHTML=getAdress;
 
 
-// 5-masala
+var getLanguage = prompt('Please write your native language');
 
-// var a = prompt('Sonni kiriting');
-// var b = a/10;
-// var c = a%10;
-// alert(b+c);
+var language = document.getElementsByClassName('language__title');
 
-// 6-masala
+language[0].innerHTML = getLanguage;
 
-// var a = prompt('Sonni kiriting');
-// var b = a/10;
-// var c = a%10;
-// alert(b*c);
+var getLanguage2 = prompt('Please write which foreign language do you know');
 
-// 7-masala
+var language2 = document.getElementsByClassName('language__title2');
 
-// var a = prompt('Sonni kiriting');
-
-// var b = a / 100;
-// alert(b);
-
-// 8-masala
-
-// var a = prompt('nechi sekund');
-// var m = a/60;
-// var s= a/3600;
-// alert(a+'sekund = ' + m + 'minutga');
-// alert(a+'sekund = ' + s + 'soatga');
-
-
-// 9-masala
-
-// var a= prompt('sonni kiriting');
-
-// if (a > 0){
-//     alert(a + 'soni musbat');
-// }
-
-// else if (a<0){
-//     alert(a+'soni manfiy');
-// }
-
-// else{
-//     alert('Bunday son mavjud emas')
-// }
-
-
-// 10-masala
-
-// var a = prompt('Sonni kiriting');
-
-// if(a%2==0){
-//     alert(a + 'soni juft');
-// }
-
-// else if (a%2!=0){
-//     alert(a + 'soni toq');
-// }
-
-// else{
-//     alert('Bunday son mavjud emas');
-// }
-
-// 11-masala
-
-// var a= prompt('Birinchi sonni kiriting');
-// var b= prompt('Ikkinchi sonni kiriting');
-
-// var c= a-b;
-
-// if (a>0 && b>0){
-//     alert(c);
-// }
-
-// else{
-//     alert('Xato son kiritilindi');
-// }
-
-// 12-masala
-
-// var a = prompt('A Sonni kiriting');
-// var b = prompt('B Sonni kiriting');
-// var c = prompt('C Sonni kiriting');
-
-// if(a>b&&b>c){
-//     alert('A>B>C')
-// }
-// else{
-//     alert('Sonlar ketma-ketligi xato');
-// }
-
-
-// 13-masala
-
-// var a = prompt('A Sonni kiriting');
-// var b = prompt('B Sonni kiriting');
-// var c = prompt('C Sonni kiriting');
-
-// if(a>b&&b<c){
-//     alert('B soni A sondan katta va C sondan kichik: A>B va B<A');
-// }
-// else{
-//     alert('Sonlar ketma-ketligi xato');
-// }
-
-// 14-masala
-
-// var a = prompt('A Sonni kiriting');
-// var b = prompt('B Sonni kiriting');
-
-// if(a%2!=0 ){
-//     alert('Kiritilgan son toq')
-// }
-// else if(b%2!=0){
-//     alert('Kiritilgan son toq');
-// }else{
-//     alert('Kiritilgan son toq emas');
-// }
-
-// 15-masala
-
-
-// var a = prompt('A Sonni kiriting');
-// var b = prompt('B Sonni kiriting');
-// var c = prompt('C Sonni kiriting');
-// var d = prompt('D Sonni kiriting');
-
-
-// if(a%2==0 && b%2==0 && c%2==0 && d%2==0){
-//     alert('Kiritilgan son juft');
-// }
-// else{
-//     alert('Kiritilgan son juft emas');
-// }
-
-
-
-// 16-masala
-
-
-// var a = prompt('A Sonni kiriting');
-// var b = prompt('B Sonni kiriting');
-// var c = prompt('C Sonni kiriting');
-
-
-
-// if(a%2==0 && b%2==0 | b%2==0 && c%2==0 | a%2==0 && c%2==0){
-//     alert('Kiritilgan son juft');
-// }
-// else{
-//     alert('Kiritilgan son juft emas');
-// }
-
-
-// 17-masala
-
-// var a  = prompt('Sonni kiriting');
-
-// if(a==1){
-//     alert('Dushanba');
-// }else if (a==2){
-//     alert('Seshanba');
-// }else if (a==3){
-//     alert('Chorshanba');
-// }else if (a==4){
-//     alert('Payshanba');
-// }else if (a==5){
-//     alert('Juma');
-// }else if(a==6){
-//     alert('Shanba');
-// }else if(a==7){
-//     alert('Yakshanba');
-// }else{
-//     alert('Xato son kiritdingiz');
-// }
+language2[0].innerHTML = getLanguage2;
